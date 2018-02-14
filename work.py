216067555873e97e7953a18b4069b20f18634490
Jenny Gao <jgao658@gmail.com>
2018-02-14 13:56:51 -0500

    zipcode, grade, and borough working

--- work.py
+++ work.py
@@ -1,9 +1,31 @@
+import pymongo
+from pprint import pprint
+
 connection = pymongo.MongoClient('149.89.150.100')
 db = connection.test
 collection = db.restaurants
 
 def boroughSearch(borough):
     results = collection.find({'borough' : borough})
-    return results
+    a=[]
+    for i in results:
+        a.append(i)
+    return pprint(a)
 
-print boroughSearch('Manhattan')
+def zipCodeSearch(zipcode):
+    results=collection.find({'address.zipcode':zipcode})
+    a=[]
+    for i in results:
+        a.append(i)
+    return pprint(a)
+
+def zipcodeGrade(zipcode, grade):
+    results=collection.find({'address.zipcode':zipcode, 'grades.grade':grade})
+    a=[]
+    for i in results:
+        a.append(i)
+    return pprint(a)
+
+#print boroughSearch('Manhattan')
+#print zipCodeSearch('10017')
+print zipcodeGrade('10017', 'B')
